4a1f87fb421ccdf8af6c31323aff1699c8dcad59
Desiree Robinson <co30@outlook.com>
2015-03-04 10:55:36 -0700

    Name

---
+++ bootstrap/js/main.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    var red = [2, 130, 73];
+    var orange = [25, 11, 50];
+    var green = [135, 10, 60];
+    var blue = [206, 17, 45];
+    var purple = [25, 55, 66];
+
+    var myName = "Desiree";
+    var letterColors = [red, orange, green, blue, purple];
+    var bubbleShape = "square";
+
+    drawName(myName, letterColors);
+    bounceName();
+    bounceBubbles();
+});
